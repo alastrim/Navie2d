fbb3a887b1f7061383cc1fbe149b94b65a0e4e52
Alastrim <nik_kalin@mail.ru>
2019-04-07 16:17:16 +0300

    more ignore

--- loop.h
+++
@@ -1,8 +0,0 @@
-#ifndef LOOP_H
-#define LOOP_H
-
-#include "misc.h"
-
-void time_loop (trio &essential);
-
-#endif // LOOP_H
--- printers.h
+++
@@ -1,9 +0,0 @@
-#ifndef PRINTERS_H
-#define PRINTERS_H
-
-#include "misc.h"
-
-void print_to_gnuplot (discrete_function &df, std::string name);
-void print_to_gnuplot (timed_discrete_function &tdf, std::string name);
-
-#endif // PRINTERS_H
