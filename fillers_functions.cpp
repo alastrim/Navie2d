725af807a10b2d646e3f1f90ed9eb8842f2cdd06
Alastrim <nik_kalin@mail.ru>
2019-04-14 21:12:51 +0300

    FIX: better fillers

--- fillers_functions.cpp
+++ fillers_functions.cpp
@@ -103,14 +103,14 @@ double f_first (double t, double x, double y)
 }
 double f_second (double t, double x, double y)
 {
-  return r (t, x, y) * (du1dt (t, x, y) + u1 (t, x, y) * du1dx1 (t, x, y) + u2 (t, x, y) * du1dx2 (t, x, y))
+  return (r (t, x, y) * (du1dt (t, x, y) + u1 (t, x, y) * du1dx1 (t, x, y) + u2 (t, x, y) * du1dx2 (t, x, y))
       + dpdx1 (t, x, y)
-      -MIU * (4.0/3.0 * ddu1dx1dx1 (t, x, y) + ddu1dx2dx2 (t, x, y) + 1.0/3.0 * ddu2dx1dx2 (t, x, y));
+      - MIU * (4.0/3.0 * ddu1dx1dx1 (t, x, y) + ddu1dx2dx2 (t, x, y) + 1.0/3.0 * ddu2dx1dx2 (t, x, y))) / r (t, x, y);
 }
 double f_third (double t, double x, double y)
 {
-  return r (t, x, y) * (du2dt (t, x, y) + u1 (t, x, y) * du2dx1 (t, x, y) + u2 (t, x, y) * du2dx2 (t, x, y))
+  return (r (t, x, y) * (du2dt (t, x, y) + u1 (t, x, y) * du2dx1 (t, x, y) + u2 (t, x, y) * du2dx2 (t, x, y))
       + dpdx2 (t, x, y)
-      -MIU * (4.0/3.0 * ddu2dx2dx2 (t, x, y) + ddu2dx1dx1 (t, x, y) + 1.0/3.0 * ddu1dx1dx2 (t, x, y));
+      - MIU * (4.0/3.0 * ddu2dx2dx2 (t, x, y) + ddu2dx1dx1 (t, x, y) + 1.0/3.0 * ddu1dx1dx2 (t, x, y))) / r (t, x, y);
 }
 }
