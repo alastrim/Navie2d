7529e5cc83d71ce295de23899568e2a6b5f771a9
Alastrim <nik_kalin@mail.ru>
2019-04-14 21:04:45 +0300

    FIX: add missing addend

--- fillers_functions.cpp
+++ fillers_functions.cpp
@@ -24,6 +24,30 @@ double p (double t, double x, double y)
 {
   return pow (r (t, x, y), GAMMA);
 }
+double ddu1dx1dx1 (double t, double x, double y)
+{
+  return - 4 * M_PI * M_PI * sin (2.0 * M_PI * x) * sin (2.0 * M_PI * y) * exp (t);
+}
+double ddu1dx1dx2 (double t, double x, double y)
+{
+  return 4 * M_PI * M_PI * cos (2.0 * M_PI * x) * cos (2.0 * M_PI * y) * exp (t);
+}
+double ddu1dx2dx2 (double t, double x, double y)
+{
+  return - 4 * M_PI * M_PI * sin (2.0 * M_PI * x) * sin (2.0 * M_PI * y) * exp (t);
+}
+double ddu2dx1dx1 (double t, double x, double y)
+{
+  return - 4 * M_PI * M_PI * sin (2.0 * M_PI * x) * sin (2.0 * M_PI * y) * exp (-t);
+}
+double ddu2dx1dx2 (double t, double x, double y)
+{
+  return 4 * M_PI * M_PI * cos (2.0 * M_PI * x) * cos (2.0 * M_PI * y) * exp (-t);
+}
+double ddu2dx2dx2 (double t, double x, double y)
+{
+  return - 4 * M_PI * M_PI * sin (2.0 * M_PI * x) * sin (2.0 * M_PI * y) * exp (-t);
+}
 double du1dx1 (double t, double x, double y)
 {
   return 2 * M_PI * cos (2.0 * M_PI * x) * sin (2.0 * M_PI * y) * exp (t);
@@ -79,10 +103,14 @@ double f_first (double t, double x, double y)
 }
 double f_second (double t, double x, double y)
 {
-  return r (t, x, y) * (du1dt (t, x, y) + u1 (t, x, y) * du1dx1 (t, x, y) + u2 (t, x, y) * du1dx2 (t, x, y)) + dpdx1 (t, x, y);
+  return r (t, x, y) * (du1dt (t, x, y) + u1 (t, x, y) * du1dx1 (t, x, y) + u2 (t, x, y) * du1dx2 (t, x, y))
+      + dpdx1 (t, x, y)
+      -MIU * (4.0/3.0 * ddu1dx1dx1 (t, x, y) + ddu1dx2dx2 (t, x, y) + 1.0/3.0 * ddu2dx1dx2 (t, x, y));
 }
 double f_third (double t, double x, double y)
 {
-  return r (t, x, y) * (du2dt (t, x, y) + u1 (t, x, y) * du2dx1 (t, x, y) + u2 (t, x, y) * du2dx2 (t, x, y)) + dpdx2 (t, x, y);
+  return r (t, x, y) * (du2dt (t, x, y) + u1 (t, x, y) * du2dx1 (t, x, y) + u2 (t, x, y) * du2dx2 (t, x, y))
+      + dpdx2 (t, x, y)
+      -MIU * (4.0/3.0 * ddu2dx2dx2 (t, x, y) + ddu2dx1dx1 (t, x, y) + 1.0/3.0 * ddu1dx1dx2 (t, x, y));
 }
 }
